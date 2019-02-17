cd4ab3d79325ab007f5041f7de540a6c4ca7b2ec
Lathe Abusaid <labusaid@gmail.com>
2019-02-16 18:02:27 -0600

    Refactored Package

--- app/src/main/java/com/latheabusaid/hackdfw2019/MainActivity.java
+++ app/src/main/java/com/latheabusaid/hackdfw2019/MainActivity.java
@@ -1,21 +1,7 @@
 package com.latheabusaid.hackdfw2019;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-
-import com.example.hackdfw2019.R;
-
-import org.altbeacon.beacon.BeaconManager;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/latheabusaid/hackdfw2019/RangingActivity.java
+++ app/src/main/java/com/latheabusaid/hackdfw2019/RangingActivity.java
@@ -3,28 +3,14 @@ package com.latheabusaid.hackdfw2019;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.RemoteException;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import java.util.Collection;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-
-import com.example.hackdfw2019.R;
 
 import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
 import org.altbeacon.beacon.BeaconManager;
-import org.altbeacon.beacon.BeaconParser;
 import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
-import org.altbeacon.beacon.powersave.BackgroundPowerSaver;
-import org.altbeacon.beacon.startup.RegionBootstrap;
-import org.altbeacon.beacon.startup.BootstrapNotifier;
-import org.altbeacon.beacon.distance.DistanceCalculator;
 
 public class RangingActivity extends Activity implements BeaconConsumer {
     protected static final String TAG = "RangingActivity";
